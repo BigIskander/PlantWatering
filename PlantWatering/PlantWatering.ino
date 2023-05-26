7cd74423e7aa27d311579dd4970578f3a6e91fdc
BigIskander <BigIskander@gmail.com>
2023-05-26 22:33:26 +0300

    Add the watering part.

--- PlantWatering/PlantWatering.ino
+++ PlantWatering/PlantWatering.ino
@@ -2,10 +2,13 @@
 #include <PubSubClient.h>
 #include "credentials.hpp" //file with WIFI and MQTT credentials
 
+//MQTT - topic names
 bool retain = true;
 char topic_voltage[] = "voltage";
 char topic_moisture[] = "moisture";
+char topic_water[] = "water";
 
+//pin allocation
 int voltage_pin = D5;
 int moisture_pin = D6;
 int motor_pin = D2;
@@ -14,11 +17,14 @@ int analog_pin = A0;
 float voltage = 0;
 float moisture = 0;
 float moisture_percent = 0;
+float water = 0;
 
 float max_moisture = 415; //422, 416, 415, 358
 float max_moisture_adj = 415;
 float min_moisture = 724; //724, 723, 721, 720
 //measured for 4.12v
+
+//int test=0;
 
 void setup()
 {
@@ -29,7 +35,10 @@ void setup()
   pinMode(moisture_pin, OUTPUT);
   pinMode(motor_pin, OUTPUT);
   pinMode(analog_pin, INPUT);
-
+  
+  //to test
+  pinMode(D8, INPUT);
+  
   //Off all transistors
   digitalWrite(voltage_pin, LOW);
   digitalWrite(moisture_pin, LOW);
@@ -51,6 +60,15 @@ void setup()
   max_moisture_adj = max_moisture - ((4.12 - voltage) * 10) * 65; 
   moisture_percent = ((min_moisture - moisture) / (min_moisture - max_moisture_adj)) * 100;
 
+  //water the plant if needed
+  water = 0;
+  if(moisture_percent < 20 && voltage >= 3.7) {
+    digitalWrite(motor_pin, HIGH);
+    delay(8.33*1000); // 300ml (pump speed 360ml in 10 sec)
+    digitalWrite(motor_pin, LOW);
+    water = 1;
+  }
+
   //Test
   //pinMode(BUILTIN_LED, OUTPUT);
 
@@ -67,7 +85,9 @@ void setup()
   client.setServer(mqtt_server, mqtt_port);
   String clientId = "WemosD1-" + WiFi.macAddress();
   if (client.connect(clientId.c_str(), mqtt_user, mqtt_password) ) {
-    if(client.publish(topic_voltage, String(voltage).c_str(), true) && client.publish(topic_moisture, String(moisture_percent).c_str(), true)) {
+    if(client.publish(topic_voltage, String(voltage).c_str(), true) 
+    && client.publish(topic_moisture, String(moisture_percent).c_str(), true) 
+    && client.publish(topic_water, String(water).c_str(), true)) {
       Serial.println("Data sent.");
     } else {
       Serial.println("Can't send data.");
@@ -85,5 +105,13 @@ void setup()
 
 void loop()
 {
-
+  /*//applied for test only
+  if(digitalRead(D8)==HIGH && test==0) {
+    test=1;
+    digitalWrite(BUILTIN_LED, HIGH);
+    digitalWrite(motor_pin, HIGH);
+    delay(8.33*1000); // 300ml (pump speed 360ml in 10 sec)
+    digitalWrite(motor_pin, LOW);
+  }
+  */
 }
